dc4d042d9442aef4edd1ebf2a819c61846d82efa
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-25 16:04:11 +0900

    Add handle intent

--- app/src/main/java/com/example/sharingsimpledata/MainActivity.java
+++ app/src/main/java/com/example/sharingsimpledata/MainActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            } else if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendMultipleImages(intent); // Handle multiple images being sent
+            }
+        } else {
+            // Handle other intents, such as being started from the home screen
+        }
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            TextView textView = (TextView) findViewById(R.id.hello_text);
+            textView.setText(sharedText);
+        }
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            ImageView imageView1 = (ImageView) findViewById(R.id.image_view_1);
+            imageView1.setImageURI(imageUri);
+        }
+    }
+
+    void handleSendMultipleImages(Intent intent) {
+        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (imageUris != null) {
+            ImageView imageView1 = (ImageView) findViewById(R.id.image_view_1);
+            imageView1.setImageURI(imageUris.get(0));
+            ImageView imageView2 = (ImageView) findViewById(R.id.image_view_2);
+            imageView2.setImageURI(imageUris.get(1));
+        }
     }
 
     public void sendText(View view) {
